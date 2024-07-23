d8c09b5d573ad14b69643adba7124b4bb3b36428
Rehan Tariq <rehantariq1858@gmail.com>
2024-07-23 15:28:01 +0500

    form update

--- components/c-o-n-t-a-c-t-form.tsx
+++ components/c-o-n-t-a-c-t-form.tsx
@@ -17,70 +17,74 @@ const CONTACTForm: NextPage<CONTACTFormType> = ({ className = "" }) => {
           <div className="self-stretch relative text-xl leading-[150%] text-gray-200 mq450:text-base mq450:leading-[24px]">{`We look forward to connecting with you soon. `}</div>
         </div>
         <form onSubmit={handleFormSubmit}>
-        <div className="self-stretch flex flex-col items-center justify-start gap-[24px] max-w-full text-base">
-          <div className="self-stretch flex flex-row items-center justify-start gap-[32px] mq750:flex-wrap mq750:gap-[16px]">
-            <div className="flex-1 flex flex-col items-start justify-start gap-[8px] min-w-[162px]">
-              <div className="self-stretch relative leading-[150%] font-medium">
-                First name
+          <div className="self-stretch flex flex-col items-center justify-start gap-[24px] max-w-full text-base">
+            <div className="self-stretch flex flex-row items-center justify-start gap-[32px] mq750:flex-wrap mq750:gap-[16px]">
+              <div className="flex-1 flex flex-col items-start justify-start gap-[8px] min-w-[162px]">
+                <div className="self-stretch relative leading-[150%] font-medium">
+                  First name
+                </div>
+                <div className="self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg bg-white flex flex-row items-start justify-start py-2.5 px-[15px] border-[1px] border-solid border-gainsboro-200">
+                  <input
+                    className="w-full [border:none] [outline:none] font-medium font-small-text text-base bg-[transparent] h-6 flex-1 relative leading-[150%] text-gray-200 text-left inline-block min-w-[131px] p-0"
+                    placeholder="Jane"
+                    id="first_name"
+                    type="text"
+                    defaultValue={""}
+                    required
+                  />
+                </div>
               </div>
-              <div className="self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg bg-white flex flex-row items-start justify-start py-2.5 px-[15px] border-[1px] border-solid border-gainsboro-200">
+              <div className="flex-1 flex flex-col items-start justify-start gap-[8px] min-w-[162px]">
+                <div className="self-stretch relative leading-[150%] font-medium">
+                  Last name
+                </div>
+                <div className="self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg bg-white flex flex-row items-start justify-start py-2.5 px-[15px] border-[1px] border-solid border-gainsboro-200">
+                  <input
+                    className="w-full [border:none] [outline:none] font-medium font-small-text text-base bg-[transparent] h-6 flex-1 relative leading-[150%] text-gray-200 text-left inline-block min-w-[131px] p-0"
+                    placeholder="Smitherton"
+                    id="last_name"
+                    defaultValue={""}
+                    type="text"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="self-stretch flex flex-col items-start justify-start gap-[8px] max-w-full">
+              <div className="self-stretch relative leading-[150%] font-medium">
+                Email address
+              </div>
+              <div className="self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg bg-white box-border flex flex-row items-start justify-start py-2.5 px-[15px] max-w-full border-[1px] border-solid border-gainsboro-200">
                 <input
-                  className="w-full [border:none] [outline:none] font-medium font-small-text text-base bg-[transparent] h-6 flex-1 relative leading-[150%] text-gray-200 text-left inline-block min-w-[131px] p-0"
-                  placeholder="Jane"
-                  id="first_name"
-                  type="text"
+                  className="w-full [border:none] [outline:none] font-medium font-small-text text-base bg-[transparent] h-6 flex-1 relative leading-[150%] text-gray-200 text-left inline-block min-w-[250px] whitespace-nowrap max-w-full p-0"
+                  placeholder="email@janesfakedomain.net"
+                  id="email"
+                  defaultValue={""}
+                  type="email"
                   required
                 />
               </div>
             </div>
-            <div className="flex-1 flex flex-col items-start justify-start gap-[8px] min-w-[162px]">
+            <div className="self-stretch flex flex-col items-start justify-start gap-[8px]">
               <div className="self-stretch relative leading-[150%] font-medium">
-                Last name
+                Your message
               </div>
-              <div className="self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg bg-white flex flex-row items-start justify-start py-2.5 px-[15px] border-[1px] border-solid border-gainsboro-200">
-                <input
-                  className="w-full [border:none] [outline:none] font-medium font-small-text text-base bg-[transparent] h-6 flex-1 relative leading-[150%] text-gray-200 text-left inline-block min-w-[131px] p-0"
-                  placeholder="Smitherton"
-                  id="last_name"
-                  type="text"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="self-stretch flex flex-col items-start justify-start gap-[8px] max-w-full">
-            <div className="self-stretch relative leading-[150%] font-medium">
-              Email address
-            </div>
-            <div className="self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg bg-white box-border flex flex-row items-start justify-start py-2.5 px-[15px] max-w-full border-[1px] border-solid border-gainsboro-200">
-              <input
-                className="w-full [border:none] [outline:none] font-medium font-small-text text-base bg-[transparent] h-6 flex-1 relative leading-[150%] text-gray-200 text-left inline-block min-w-[250px] whitespace-nowrap max-w-full p-0"
-                placeholder="email@janesfakedomain.net"
-                id="email"
-                type="email"
+              <textarea
+                className="bg-white h-[162px] w-auto [outline:none] self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg box-border flex flex-row items-start justify-start py-3 px-4 font-small-text font-medium text-base text-gray-200 border-[1px] border-solid border-gainsboro-200"
+                placeholder="Enter your question or message"
+                id="project_details"
+                defaultValue={""}
                 required
+                rows={8}
+                cols={27}
               />
             </div>
+            <button className="cursor-pointer [border:none] py-2 px-6 bg-black1 shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg flex flex-row items-center justify-center hover:bg-darkslategray-200">
+              <div className="relative text-xl leading-[150%] font-medium font-small-text text-white text-left inline-block min-w-[68px] mq450:text-base mq450:leading-[24px]">
+                Submit
+              </div>
+            </button>
           </div>
-          <div className="self-stretch flex flex-col items-start justify-start gap-[8px]">
-            <div className="self-stretch relative leading-[150%] font-medium">
-              Your message
-            </div>
-            <textarea
-              className="bg-white h-[162px] w-auto [outline:none] self-stretch shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg box-border flex flex-row items-start justify-start py-3 px-4 font-small-text font-medium text-base text-gray-200 border-[1px] border-solid border-gainsboro-200"
-              placeholder="Enter your question or message"
-              id="project_details"
-              required
-              rows={8}
-              cols={27}
-            />
-          </div>
-          <button className="cursor-pointer [border:none] py-2 px-6 bg-black1 shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] rounded-lg flex flex-row items-center justify-center hover:bg-darkslategray-200">
-            <div className="relative text-xl leading-[150%] font-medium font-small-text text-white text-left inline-block min-w-[68px] mq450:text-base mq450:leading-[24px]">
-              Submit
-            </div>
-          </button>
-        </div>
-      </form>
+        </form>
       </div>
       <img
         className="w-[428px] relative max-h-full object-contain max-w-full lg:flex-1"
--- components/handleFormSubmit.tsx
+++ components/handleFormSubmit.tsx
@@ -4,28 +4,39 @@ import { getDatabase, ref, set } from "../node_modules/firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC_7RzeZO5ll8iF0_0heXpTb5Dhra2fcOE",
-    authDomain: "williams-plumbing-solutions.firebaseapp.com",
-    projectId: "williams-plumbing-solutions",
-    storageBucket: "williams-plumbing-solutions.appspot.com",
-    messagingSenderId: "757654487126",
-    appId: "1:757654487126:web:d1aef59d2bc73155e17bf7"
-  };
+  apiKey: "AIzaSyC_7RzeZO5ll8iF0_0heXpTb5Dhra2fcOE",
+  authDomain: "williams-plumbing-solutions.firebaseapp.com",
+  projectId: "williams-plumbing-solutions",
+  storageBucket: "williams-plumbing-solutions.appspot.com",
+  messagingSenderId: "757654487126",
+  appId: "1:757654487126:web:d1aef59d2bc73155e17bf7",
+};
 
 // Initialize Firebase
 const fireBaseApp = initializeApp(firebaseConfig);
 const db = getDatabase(fireBaseApp);
 
 // handleForm.js
-export const handleFormSubmit = (event : any) => {
+export const handleFormSubmit = (event: any) => {
   console.log("Form submitted");
   // Add your form handling logic here
   event.preventDefault();
 
-  const first_name = document.getElementById("first_name").value;
-  const last_name = document.getElementById("last_name").value;
-  const email = document.getElementById("email").value;
-  const project_details = document.getElementById("project_details").value;
+  const firstNameInput = document.getElementById(
+    "first_name"
+  ) as HTMLInputElement;
+  const lastNameInput = document.getElementById(
+    "last_name"
+  ) as HTMLInputElement;
+  const emailInput = document.getElementById("email") as HTMLInputElement;
+  const projectDetailsInput = document.getElementById(
+    "project_details"
+  ) as HTMLTextAreaElement;
+
+  const first_name = firstNameInput.value;
+  const last_name = lastNameInput.value;
+  const email = emailInput.value;
+  const project_details = projectDetailsInput.value;
 
   const data = {
     first_name,
@@ -41,7 +52,6 @@ export const handleFormSubmit = (event : any) => {
   // Write data to this reference
   set(userRef, data)
     .then(() => {
-    //   alert("Form submitted successfully!");
       console.log("Form submitted successfully");
     })
     .catch((error) => {
@@ -49,5 +59,5 @@ export const handleFormSubmit = (event : any) => {
       alert("Error submitting form: " + error.message);
     });
 
-    event.target.reset();
+  event.target.reset();
 };
